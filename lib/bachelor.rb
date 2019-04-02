edcdde86ee8f100994c7375f5dda4d034ea9bc76
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-02 22:20:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |season, all_contestants|
     all_contestants.each do |contestant|
       if contestant["status"] == "Winner"
-        return (contestant["name"].split(" ")[0])
+        return contestant["name"].split(" ")[0].to_s
     end
   end
 end
