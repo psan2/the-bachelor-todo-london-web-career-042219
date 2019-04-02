4eaf87bc705d5de7e0d37c6296398bcdbc038892
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-02 22:36:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,5 +57,5 @@ def get_average_age_for_season(data, season)
   end
   avg_age = total_years / num_contestants
   decimal = avg_age % 1
-  decimal >= .5 ? (avg_age + (1 - decimal)) : (avg_age - decimal)
+  decimal >= 0.5 ? (avg_age + (1 - decimal)) : (avg_age - decimal)
 end
